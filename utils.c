6ea017b17630145c92fedd31fed1f41df7ce40e1
Flora De Checchi <flora.dechecchi@gmail.com>
2026-09-04 09:17:50 +0200

    division de la parrie simulator en plusieur fonctions dans coders et dongles, sattaquer a la partie routine _fucnbtion

--- utils.c
+++ utils.c
@@ -6,6 +6,7 @@ void *routine_function(void *arg)
     t_coder *coder;
 
     coder = (t_coder *)arg;
-    printf("%lu\n", coder->thread);
+    printf("%p\n", (void *)coder->thread);
+    printf("%lu\n", (unsigned long)coder->thread);
     return (NULL);
 }
